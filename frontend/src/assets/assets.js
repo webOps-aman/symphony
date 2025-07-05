1708fd4a1e9ae8e310f34ccfb2041a1344daa6d7
webOps-aman <amanhxe@gmail.com>
2025-07-05 23:01:14 +0530

    assets file updated

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,84 @@
+import product_img1_1 from './product_img1_1.png';
+import product_img1_2 from './product_img1_2.png';
+import product_img1_3 from './product_img1_3.png';
+import product_img2 from './product_img2.png';
+import product_img3 from './product_img3.png';
+import product_img4_1 from './product_img4_1.png';
+import product_img4_2 from './product_img4_2.png';
+import product_img5 from './product_img5.png';
+import product_img6_1 from './product_img6_1.png';
+import product_img6_2 from './product_img6_2.png';
+import product_img7 from './product_img7.png';
+import product_img8 from './product_img8.png';
+import product_img9 from './product_img9.png';
+import product_img10_1 from './product_img10_1.png';
+import product_img10_2 from './product_img10_2.png';
+import product_img10_3 from './product_img10_3.png';
+import product_img11_1 from './product_img11_1.png';
+import product_img11_2 from './product_img11_2.png';
+import product_img12 from './product_img12.png';
+import product_img13 from './product_img13.png';
+import product_img14_1 from './product_img14_1.png';
+import product_img14_2 from './product_img14_2.png';
+import product_img15 from './product_img15.png';
+import product_img16_1 from './product_img16_1.png';
+import product_img16_2 from './product_img16_2.png';
+import product_img16_3 from './product_img16_3.png';
+import product_img17 from './product_img17.png';
+import product_img18_1 from './product_img18_1.png';
+import product_img18_2 from './product_img18_2.png';
+import product_img18_3 from './product_img18_3.png';
+import product_img18_4 from './product_img18_4.png';
+import product_img19 from './product_img19.png';
+import product_img20 from './product_img20.png';
+import product_img21 from './product_img21.png';
+import product_img22 from './product_img22.png';
+import product_img23 from './product_img23.png';
+import product_img24 from './product_img24.png';
+import product_img25 from './product_img25.png';
+import product_img26 from './product_img26.png';
+import product_img27 from './product_img27.png';
+import product_img28 from './product_img28.png';
+import product_img29 from './product_img29.png';
+import product_img30 from './product_img30.png';
+
+import header_banner1 from './header_banner1.jpeg';
+import header_banner2 from './header_banner2.jpeg';
+import header_banner3 from './header_banner3.jpeg';
+import header_banner4 from './header_banner4.jpeg';
+import header_banner5 from './header_banner5.jpeg';
+import header_banner6 from './header_banner6.jpeg';
+import header_banner7 from './header_banner7.png';
+import header_banner8 from './header_banner8.png';
+
+import bladeless_tower_fan1 from './bladeless_tower_fan1.png';
+import bladeless_tower_fan2 from './bladeless_tower_fan2.png';
+import bladeless_tower_fan3 from './bladeless_tower_fan3.png';
+import bladeless_tower_fan4 from './bladeless_tower_fan4.png';
+import bladeless_tower_fan5 from './bladeless_tower_fan5.png';
+
+import indoor_air_cooler from './indoor_air_cooler.png';
+import indoor_air_cooler1 from './indoor_air_cooler1.png';
+import outdoor_air_cooler from './outdoor_air_cooler.png';
+import outdoor_air_cooler1 from './outdoor_air_cooler1.png';
+import industrial_commercial_cooler from './industrial_commercial_cooler.png';
+import industrial_commercial_cooler1 from './industrial_commercial_cooler1.png';
+
+import banner1 from './banner1.jpeg';
+import banner2 from './banner2.jpeg';
+import banner3 from './banner3.jpeg';
+import banner4 from './banner4.png';
+import banner5 from './banner5.jpeg';
+
+import blog_image1 from './blog_image1.jpeg';
+import blog_image2 from './blog_image2.jpeg';
+import blog_image3 from './blog_image3.jpeg';
+import blog_image4 from './blog_image4.jpeg';
+import blog_image5 from './blog_image5.jpeg';
+import blog_image6 from './blog_image6.jpeg';
+import blog_image7 from './blog_image7.jpeg';
+import blog_image8 from './blog_image8.jpeg';
+
+import logo1 from './logo1.png';
+
+
